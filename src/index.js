5b89c2857d09230120e6fae7c3014a332c63c205
Hanh <hanhlv.21it@vku.udn.vn>
2022-08-01 15:55:40 +0700

    1

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express') // method 
+const morgan = require('morgan');
+const path = require('path'); 
+const handlebars = require('express-handlebars');
+const app = express(); // đối tượng nhận phương thức 
+
+const port = 3000; 
+
+// static file
+app.use(express.static(path.join(__dirname, 'public'))); 
+console.log(path.join(__dirname, 'public')); 
+
+
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// template engine
+app.engine('hbs', handlebars.engine({
+  extname: ".hbs"
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'))
+
+
+// routing : lộ trình đi 
+app.get('/', (req, res) => {// sử dụng các phương thức của đối tượng / là đường dẫn khi vào trag đầu tiên 
+
+   res.render('home');  
+
+})
+app.get('/news', (req, res) => {// sử dụng các phương thức của đối tượng / là đường dẫn khi vào trag đầu tiên 
+
+  res.render('news');  
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
